9c50a55840f9e0a9241da57ef605f72a0335332a
Milad <miladsalami1385@gmail.com>
2024-11-08 13:46:36 -0800

    add post model

---
+++ src/models/Post.js
@@ -0,0 +1,31 @@
+const mongoose = require("mongoose");
+
+const schema = new mongoose.Schema(
+  {
+    media: {
+      filename: {
+        type: String,
+        required: true,
+      },
+      path: {
+        type: String,
+        required: true,
+      },
+    },
+    description: {
+      type: String,
+      required: true,
+    },
+
+    hashtags: [String],
+    user: {
+      type: mongoose.Types.ObjectId,
+      required: true,
+    },
+  },
+  { timestamps: true }
+);
+
+const model = mongoose.model("Post", schema);
+
+module.exports = model;
