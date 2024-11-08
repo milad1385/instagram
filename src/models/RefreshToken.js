71d46bb273ff0cb14b81076aba1e3f8fbe4219aa
Milad <miladsalami1385@gmail.com>
2024-11-08 13:24:41 -0800

    add statics method for refresh token

---
+++ src/models/RefreshToken.js
@@ -0,0 +1,46 @@
+const mongoose = require("mongoose");
+const UUID = require("uuid");
+const schema = new mongoose.Schema(
+  {
+    user: {
+      type: mongoose.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+    token: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    expire: {
+      type: Date,
+      reqired: true,
+    },
+  },
+  { timestamps: true }
+);
+
+schema.statics.createToken = async (user) => {
+  const expireDates = new Date() + 900_000;
+  const token = UUID.v4();
+  const createdToken = await model.create({
+    user: user._id,
+    expire: expireDates,
+    token,
+  });
+  return token;
+};
+
+schema.statics.verifyToken = async (token) => {
+  const refreshToken = await model.findOne({ token });
+
+  if (refreshToken && refreshToken.expire >= Date.now()) {
+    return refreshToken.token;
+  }
+
+  return false;
+};
+
+const model = mongoose.model("refreshToken", schema);
+
+module.exports = model;
