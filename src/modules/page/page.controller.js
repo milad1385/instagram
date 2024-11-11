6e9af0429492433cbb01212fb1fbb22c54c3e711
Milad <miladsalami1385@gmail.com>
2024-11-11 13:09:22 -0800

    add follow controller

--- src/modules/page/page.controller.js
+++ src/modules/page/page.controller.js
@@ -1,11 +1,13 @@
-const followModel = require("../../models/follow");
+const { isValidObjectId } = require("mongoose");
+const FollowModel = require("../../models/follow");
+const UserModel = require("../../models/User");
 const isAllowToSeePage = require("../../utils/isAllowToSeePage");
 
 exports.showPageView = async (req, res, next) => {
   try {
     const { pageId } = req.params;
     const hasAccessToPage = await isAllowToSeePage(req.user._id, pageId);
-    const followed = await followModel.findOne({
+    const followed = await FollowModel.findOne({
       follower: req.user._id,
       following: pageId,
     });
@@ -26,3 +28,42 @@ exports.showPageView = async (req, res, next) => {
     next(error);
   }
 };
+
+exports.follow = async (req, res, next) => {
+  try {
+    const { pageId } = req.params;
+
+    if (!isValidObjectId(pageId)) {
+      req.flash("error", "page id is not valid");
+      return res.redirect("/");
+    }
+
+    const page = await UserModel.findOne({ _id: pageId });
+
+    if (!page) {
+      req.flash("error", "page not found :(");
+      return res.redirect("/");
+    }
+
+    const existFollowing = await FollowModel.findOne({
+      follower: req.user._id,
+      following: pageId,
+    });
+
+    if (existFollowing) {
+      req.flash("error", "You have followed this user");
+      return res.redirect(`/page/${pageId}`);
+    }
+
+    await FollowModel.create({
+      follower: req.user._id,
+      following: pageId,
+    });
+
+    req.flash("success", "follow user successfully :)");
+    return res.redirect(`/page/${pageId}`);
+  } catch (error) {
+    next(error);
+  }
+};
+
--- src/modules/page/page.routes.js
+++ src/modules/page/page.routes.js
@@ -4,5 +4,7 @@ const auth = require("../../middlewares/auth");
 const router = express.Router();
 
 router.route("/:pageId").get(auth, controller.showPageView);
+router.route("/:pageId/follow").post(auth, controller.follow);
+router.route("/:pageId/unfollow").post(auth, controller.unfollow);
 
 module.exports = router;
