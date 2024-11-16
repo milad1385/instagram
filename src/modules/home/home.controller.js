9be47656e5eb2ebc23060b4c1e3552048b07ce07
Milad salamian <miladsalami1385@gmail.com>
2024-11-16 12:48:33 -0800

    get and show all posts in index page

--- src/modules/home/home.controller.js
+++ src/modules/home/home.controller.js
@@ -1,7 +1,42 @@
+const PostModel = require("../../models/Post");
+const LikeModel = require("../../models/like");
+const SaveModel = require("../../models/Save");
 exports.showHomePage = async (req, res, next) => {
   try {
     const user = req.user;
-    return res.render("index", { user });
+
+    
+    const posts = await PostModel.find({})
+      .populate("user", "name username")
+      .sort({ _id: -1 });
+
+    const likes = await LikeModel.find({ user: req.user._id }).populate(
+      "post",
+      "_id"
+    );
+
+    const saves = await SaveModel.find({ user: req.user._id }).populate(
+      "post",
+      "_id"
+    );
+
+    posts.forEach((post) => {
+      likes.forEach((like) => {
+        if (post._id.toString() === like.post._id.toString()) {
+          post.hasLike = true;
+        }
+      });
+    });
+
+    posts.forEach((post) => {
+      saves.forEach((save) => {
+        if (post._id.toString() === save.post._id.toString()) {
+          post.isSave = true;
+        }
+      });
+    });
+
+    return res.render("index", { user, posts });
   } catch (error) {
     next(error);
   }
--- src/modules/home/home.routes.js
+++ src/modules/home/home.routes.js
@@ -1,11 +1,7 @@
 const express = require("express");
 const controller = require("./home.controller");
-const auth = require("./../../middlewares/auth")
+const auth = require("./../../middlewares/auth");
 const router = express.Router();
-
-// app.get("/", (req, res) => {
-//   return res.render("index");
-// });
 
 router.route("/").get(auth, controller.showHomePage);
 
